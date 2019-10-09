27476984a0be8b3d02f1aa4a2b5ec62128be1ac7
AlexandrMel <melnic.alexandr88@gmail.com>
2019-10-09 15:25:03 +0200

    Cleaning and Commenting code

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+//Connect FS
 let fs = require("fs");
-let wordN = process.argv[2]
+// use terminal arguments
+let word = process.argv[2]
 let path = process.argv[3]
-console.log(wordN)
-
+//Create the ReadStream
 let myStream = fs.createReadStream(path, "utf8")
-console.log(myStream)
+//Define the global variables
 let chunkNumber = 1;
-let word = wordN;
 let wordCounter = 0;
 let chunkCount = 0;
 let arry = 0;
-
+// Read chunks of data with callback function
 myStream.on("data", chunk => {
-let len = chunk.length;
+//Counting Number of Chunks and numbering them
 console.log(`Reading chunk ${chunkNumber}`);
 chunkNumber++;
 chunkCount++;
-
+// count the appearance of the word in the file by chunks
 arry = arry + chunk.split(word).length-1;
 
 
 });
+//display the results using "end" type
 myStream.on("end", () => {
     console.log(`====================`);
     console.log(`End of data\nNumber of chunks: ${chunkCount}\nFound ${word} ${arry} times`)
